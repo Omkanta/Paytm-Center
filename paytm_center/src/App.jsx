91dc3ff23ea2dfb1c860c907c9d6133987842042
omkanta <1omkant1998@gmail.com>
2023-01-18 23:51:38 +0530

    day-2

---
+++ paytm_center/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import AllRoutes from './Component/AllRoutes';
+import Navbar from './Component/Navbar';
+import Home from './Pages/Home';
+import Footer from "./Component/Footer"
+
+const App=()=>{
+  return (
+    <div className="App">
+     {/* <AllRoutes/> */}
+     <Navbar/>
+     <Footer/>
+    </div>
+  );
+}
+
+export default App;
